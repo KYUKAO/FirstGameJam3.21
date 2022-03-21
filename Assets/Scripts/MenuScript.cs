482861d4d6060dcc1889f714432d77c816d8d1cb
KYUKAO <80613431+KYUKAO@users.noreply.github.com>
2022-03-21 15:36:54 +0800

    Menu

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+public void StartGame()
     {
-        
+        SceneManager.LoadScene("Level");
     }
-
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
 }
