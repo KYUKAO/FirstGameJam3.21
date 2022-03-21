623c4e644f8249cf487298f478cb9e78f841b48e
KYUKAO <80613431+KYUKAO@users.noreply.github.com>
2022-03-21 14:39:48 +0800

    主角bug修了

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -39,19 +39,27 @@ public class PlayerControl : BaseUnit
         LevelText.text="LEVEL :"+Level;
         #region MOVEMENT
         Vector3 ms = Input.mousePosition;
-            ms = Camera.main.ScreenToWorldPoint(ms);//获取鼠标相对位置
-                                                    //对象的位置
-            Vector3 gunPos = this.transform.position;
-            float fireangle;//发射角度
-                            //计算鼠标位置与对象位置之间的角度
-            Vector2 targetDir = ms - gunPos;
-            fireangle = Vector2.Angle(targetDir, Vector3.up);
-            if (ms.x > gunPos.x)
-            {
-                fireangle = -fireangle;
-            }
-            this.transform.eulerAngles = new Vector3(0, 0, fireangle);
-            Vector2 direction = ms.normalized;
+        //ms = Camera.main.ScreenToWorldPoint(ms);//获取鼠标相对位置
+        //                                        //对象的位置
+        //Vector3 gunPos = this.transform.position;
+        //float fireangle;//发射角度
+        //                //计算鼠标位置与对象位置之间的角度
+        //Vector2 targetDir = ms - gunPos;
+        //fireangle = Vector2.Angle(targetDir, Vector3.up);
+        //if (ms.x > gunPos.x)
+        //{
+        //    fireangle = -fireangle;
+        //}
+        Vector3 targetPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        float fireangle;
+        Vector2 targetDir = ms - targetPos;
+        fireangle = Vector2.Angle(targetDir, Vector3.up);
+        if (ms.x > targetPos.x)
+        {
+            fireangle = -fireangle;
+        }
+        this.transform.eulerAngles = new Vector3(0, 0, fireangle);
+            Vector2 direction = targetDir.normalized;
             rb.velocity = direction * Speed;
         if (Input.GetMouseButton(0))
         {
@@ -59,7 +67,7 @@ public class PlayerControl : BaseUnit
         }
         if (Input.GetMouseButtonUp(0))
         {
-            rb.velocity = direction * Speed * (1 - DecreaseRate);
+            rb.velocity = direction * Speed;
         }
         #endregion
 
